1a5876f207b7b41655bebf98bf71ef9e8c845038
Karen Pion <karenlpion@gmail.com>
2021-07-06 02:11:35 -0400

    toggle hide and show weather info

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,20 @@
 var cityFormInput = $('#city-form');
 var cityInputEl = $('#city');
-
-
-var fetchBtnEl = $('#fetchBtn');
+var forecastTodayEl = $('#forecastToday');
+var weatherInfoEl = $('#weatherInfo');
 
 var dateToday = moment().format('M/D/YYYY');
 console.log(dateToday)
 
+weatherInfoEl.hide();
+
+cityFormInput.on('submit', handleFormSubmit);
 
 function handleFormSubmit(event) {
     event.preventDefault();  
-    console.log('City:', cityInputEl.val())
+    console.log('City:', cityInputEl.val());
 
-    var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityInputEl.val() +'&appid=b9906dda28b3c1ed70ea82d2cbf1483d'
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityInputEl.val() +'&appid=b9906dda28b3c1ed70ea82d2cbf1483d';
 
     fetch(requestUrl)
     .then(function (response) {
@@ -20,8 +22,16 @@ function handleFormSubmit(event) {
     })
     .then(function (data) {
       console.log(data);
-    });
-    cityInputEl.val("")
-}
 
-cityFormInput.on('submit', handleFormSubmit);
+    weatherInfoEl.show();
+
+      var forecastTodayTitle = $('<h1>');
+      forecastTodayTitle.text(cityInputEl.val() + dateToday);
+      forecastTodayEl.append(forecastTodayTitle);
+
+
+    });
+    cityInputEl.val("");
+
+
+}
