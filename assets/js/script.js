7bf0a0ef84d8e8327699d94fe40649af01ca6417
Karen Pion <karenlpion@gmail.com>
2021-07-06 01:43:39 -0400

    requestURL, fetch request with user input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,27 @@
 var cityFormInput = $('#city-form');
 var cityInputEl = $('#city');
 
+
 var fetchBtnEl = $('#fetchBtn');
+
+var dateToday = moment().format('M/D/YYYY');
+console.log(dateToday)
+
 
 function handleFormSubmit(event) {
     event.preventDefault();  
     console.log('City:', cityInputEl.val())
-};
+
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityInputEl.val() +'&appid=b9906dda28b3c1ed70ea82d2cbf1483d'
+
+    fetch(requestUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+    cityInputEl.val("")
+}
 
 cityFormInput.on('submit', handleFormSubmit);
-console.log('hello')
-// function getApi(){
-// var requestUrl = 
-// }
