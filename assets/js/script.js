de9d116f2c11149e6487c96c2fbc23128d9da461
Karen Pion <karenlpion@gmail.com>
2021-07-06 03:23:29 -0400

    adding today's temp, wind, humidity

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,63 @@
+// Variables locating input areas and sections
 var cityFormInput = $('#city-form');
 var cityInputEl = $('#city');
 var forecastTodayEl = $('#forecastToday');
 var weatherInfoEl = $('#weatherInfo');
 
+// Variable for current date
 var dateToday = moment().format('M/D/YYYY');
-console.log(dateToday)
 
+// Hides weather information section on page load
 weatherInfoEl.hide();
 
+// Event handler listening for submitting the form
 cityFormInput.on('submit', handleFormSubmit);
 
 function handleFormSubmit(event) {
+    // Prevents refreshing of page
     event.preventDefault();  
     console.log('City:', cityInputEl.val());
 
-    var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityInputEl.val() +'&appid=b9906dda28b3c1ed70ea82d2cbf1483d';
+    // Url used to grab API based on city inputted
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityInputEl.val() +'&units=imperial&appid=b9906dda28b3c1ed70ea82d2cbf1483d';
 
     fetch(requestUrl)
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {
-      console.log(data);
+      console.log(data)
 
-    weatherInfoEl.show();
+        // Shows weather info section once data is generated
+        weatherInfoEl.show();
 
-      var forecastTodayTitle = $('<h1>');
-      forecastTodayTitle.text(cityInputEl.val() + dateToday);
-      forecastTodayEl.append(forecastTodayTitle);
+        // Creates, appends and adds text for today's forecast
+        var forecastTodayTitle = $('<h1>');
+        forecastTodayTitle.text(cityInputEl.val() + ' ' + dateToday);
+        forecastTodayEl.append(forecastTodayTitle);
 
+        // Creates, appends and adds text for today's temperature
+        var tempToday = $('<p>');
+        tempToday.text("Temp: " + data.main.feels_like);
+        forecastTodayEl.append(tempToday);
 
+        // Creates, appends and adds text for today's windspeed
+        var windToday= $('<p>');
+        windToday.text("Wind: " + data.wind.speed + " MPH");
+        forecastTodayEl.append(windToday);
+
+        // Creates, appends and adds text for today's humidity
+        var humidityToday = $('<p>');
+        humidityToday.text("Humidity: " + data.main.humidity + "%")
+        forecastTodayEl.append(humidityToday);
+
+        // var uvIndexToday = $('<p>');
+        // uvIndexToday.text("UV Index: " + )
+    
     });
-    cityInputEl.val("");
-
-
 }
+
+// Clears form of city input
+cityInputEl.val("");
+
+
